431f414f2a1c513799c8755c097cb64bf0f21028
ReddPhoenix <wsguerrero@gmail.com>
2020-09-28 22:46:53 -0600

    created class for engineer.js

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,22 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+
+// Engineer class requires name, id, email, github username
+// Engineer class requires getGithub(), getRole () 
+// getRole() overwritten to return 'Engineer'
+
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.github = github;
+    }
+    getGithub() {
+        return this.github;
+    }
+    getRole() {
+        return 'Engineer';
+    }
+}
+
+module.exports = Engineer;
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # template-engine-employee-summary
+
+
+
+https://www.programiz.com/java-programming/super-keyword#:~:text=As%20we%20know%2C%20when%20an,form%20of%20the%20super%20keyword  Java super
